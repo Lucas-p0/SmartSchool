130a43ff7017f33a2ad9c4c0698003b1585d34ea
Lucas pinheiro <lucaslogos@gmail.com>
2023-03-24 12:25:21 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AlunosComponent } from './alunos/alunos.component';
 import { ProfessoresComponent } from './professores/professores.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'perfil', component: PerfilComponent },
   { path: 'alunos', component: AlunosComponent },
   { path: 'professores', component: ProfessoresComponent },
+
 ];
 
 @NgModule({
