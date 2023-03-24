5e8ffbfee8274ef0634fc73a5ebd6edbb01694c3
Lucas pinheiro <lucaslogos@gmail.com>
2023-03-24 08:28:28 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
 import { ProfessoresComponent } from './professores/professores.component';
 
 const routes: Routes = [
-  { path: 'professores', component: ProfessoresComponent }
+  { path: 'alunos', component: AlunosComponent },
+  { path: 'professores', component: ProfessoresComponent },
 ];
 
 @NgModule({
