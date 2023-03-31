3079d93f002284a72f0007a212ac7b23663ebc79
Lucas pinheiro <lucaslogos@gmail.com>
2023-03-31 17:29:12 -0300

    update

--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavbarComponent } from './navbar.component';
+
+
+
+
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
--- src/app/professores/professores.component.ts
+++ src/app/professores/professores.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-professores',
   templateUrl: './professores.component.html',
